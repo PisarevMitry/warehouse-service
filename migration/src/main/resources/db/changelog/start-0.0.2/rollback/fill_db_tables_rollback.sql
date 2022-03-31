f99a58fb1401f1c5a7d10e765ad4dc9cb7c5a497
Pisarev <d.pisarev.03@mail.ru>
2022-03-31 20:29:31 +0300

    @init

---
+++ migration/src/main/resources/db/changelog/start-0.0.2/rollback/fill_db_tables_rollback.sql
@@ -0,0 +1,12 @@
+TRUNCATE TABLE person_data;
+TRUNCATE TABLE address;
+TRUNCATE TABLE illness;
+TRUNCATE TABLE medical_card;
+TRUNCATE TABLE contact;
+
+DROP SEQUENCE person_data_seq;
+DROP SEQUENCE address_seq;
+DROP SEQUENCE illness_seq;
+DROP SEQUENCE medical_card_seq;
+DROP SEQUENCE contact_seq;
+
